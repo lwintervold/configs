e08599069534701f0668c6398111640ef32953f2
Lee Wintervold <38053017+lwintervold@users.noreply.github.com>
2021-08-26 19:05:00 -0500

    Fix path

--- setup.sh
+++ setup.sh
@@ -17,4 +17,4 @@ cp $SCRIPT_PATH/vimrc ~/.vimrc
 
 # Git
 cp $SCRIPT_PATH/gitignore ~/.gitignore
-git config --global core.excludesfile CONFIG_DIR/.gitignore
+git config --global core.excludesfile ~/.gitignore
